b6f5a4293363002eb0cbde79862e8f5e3de12165
Teo Cherici <tcherici@gmail.com>
2023-03-10 17:57:03 +0100

    Base image cropping and overlay setup

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+const image = document.getElementById("image");
+const cropper = new Cropper(image, {
+    dragMode: 'move',
+    aspectRatio: 75/100,
+    autoCropArea: 0.95,
+    restore: false,
+    guides: false,
+    center: false,
+    highlight: false,
+    cropBoxMovable: false,
+    cropBoxResizable: false,
+    toggleDragModeOnDblclick: false,
+});
